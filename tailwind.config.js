b8f4ac221de454a2a7a994fb846deec89fefb1d5
Alireza Sadeghi <alirezawdx@gmail.com>
2024-03-24 01:51:54 +0330

    feat: add color pallete to tailwind.conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
     content: ["./src/**/*.{html,js}"],
     theme: {
+        collors: {
+            primary: "#D69E36",
+            "primary-accent": "#DEA02E",
+            secondary: "#F5B747",
+            "custom-brown": "#2B261D",
+            "custom-dark": "#403B45",
+        },
         extend: {},
     },
     plugins: [],
